89db2e80bc189e9a9f4d039d8569b9dbffddc473
Brad Gessler <bradgessler@gmail.com>
2024-08-29 12:26:37 -0700

    Initial Terminalwire gem commit

---
+++ lib/terminalwire.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative "terminalwire/version"
+
+module Terminalwire
+  class Error < StandardError; end
+end
---
+++ lib/terminalwire/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Terminalwire
+  VERSION = "0.1.0"
+end
