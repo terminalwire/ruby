1c0f759da7854aa334aec3dab265630963095d88
Brad Gessler <bradgessler@gmail.com>
2024-08-31 08:46:50 -0700

    Add generator to install Terminalwire

---
+++ lib/generators/terminalwire/install/install_generator.rb
@@ -0,0 +1,37 @@
+require "bundler"
+
+class Terminalwire::InstallGenerator < Rails::Generators::Base
+  source_root File.expand_path("templates", __dir__)
+
+  argument :binary_name, type: :string, required: true, banner: "binary_name"
+
+  def create_terminal_files
+    template "application_terminal.rb.tt", Rails.root.join("app/terminal/application_terminal.rb")
+  end
+
+  def create_binary_files
+    copy_file "bin/terminalwire", binary_path
+    chmod binary_path, 0755, verbose: false
+  end
+
+  def add_route
+    route <<~ROUTE
+      match "/terminal",
+        to: Terminalwire::WebSocket::ThorServer.new(ApplicationTerminal),
+        via: [:get, :connect]
+    ROUTE
+  end
+
+  def print_post_install_message
+    say ""
+    say "Terminalwire has been successfully installed!", :green
+    say "Run `#{binary_path.relative_path_from(Rails.root)}` to verify everything is in working order. For support visit https://terminalwire.com."
+    say ""
+  end
+
+  private
+
+  def binary_path
+    Rails.root.join("bin", binary_name)
+  end
+end
