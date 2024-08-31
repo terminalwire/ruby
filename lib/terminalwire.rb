df89b6a5f35ce586209a09fd4cdc94c94a727a97
Brad Gessler <bradgessler@gmail.com>
2024-08-31 08:47:01 -0700

    Ignore generators files for autoloader

--- lib/terminalwire.rb
+++ lib/terminalwire.rb
@@ -23,6 +23,7 @@ module Terminalwire
   class Error < StandardError; end
 
   Loader = Zeitwerk::Loader.for_gem.tap do |loader|
+    loader.ignore("#{__dir__}/generators")
     loader.setup
   end
 
