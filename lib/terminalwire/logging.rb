65cdee771d898efff5eb356ef832ba5c2c04f226
Brad Gessler <bradgessler@gmail.com>
2024-10-24 14:34:12 -0700

    Append TERMINALWIRE to env vars

--- lib/terminalwire/logging.rb
+++ lib/terminalwire/logging.rb
@@ -2,7 +2,7 @@ require 'logger'
 
 module Terminalwire
   module Logging
-    DEVICE = Logger.new($stdout, level: ENV.fetch("LOG_LEVEL", "info"))
+    DEVICE = Logger.new($stdout, level: ENV.fetch("TERMINALWIRE_LOG_LEVEL", "info"))
     def logger = DEVICE
   end
 end
