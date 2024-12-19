4591a72bada62068461438759da522f9d0620e54
Brad Gessler <bradgessler@gmail.com>
2024-12-19 12:25:46 -0800

    Organize builder into `./builder` directory.

--- builder/boot.rb
+++
@@ -1,7 +0,0 @@
-# Resolve the base directory
-base_dir = File.expand_path("..", __FILE__)
-
-# Add all gem paths under lib/vendor to the load path
-Dir.glob(File.join(base_dir, "vendor/gems/gems/*/lib")).each do |gem_path|
-  $LOAD_PATH.unshift gem_path
-end
