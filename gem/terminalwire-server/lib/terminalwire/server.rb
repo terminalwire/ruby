e3e463f59018642c721c94f270bf93d5da36ce1d
Brad Gessler <bradgessler@gmail.com>
2025-01-31 16:20:51 -0800

    Move WebSocket to its own file

--- gem/terminalwire-server/lib/terminalwire/server.rb
+++ gem/terminalwire-server/lib/terminalwire/server.rb
@@ -8,20 +8,5 @@ end
 
 module Terminalwire
   module Server
-    class WebSocket
-      include Logging
-
-      def call(env)
-        Async::WebSocket::Adapters::Rack.open(env, protocols: ['ws']) do |connection|
-          handle(Adapter::Socket.new(Terminalwire::Transport::WebSocket.new(connection)))
-        end or [200, { "Content-Type" => "text/plain" }, ["Connect via WebSockets"]]
-      end
-
-      def handle(adapter)
-        while message = adapter.read
-          puts message
-        end
-      end
-    end
   end
 end
